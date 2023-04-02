a4e362448877597ec104e022299bdb053deb863e
quincynash <quincy.nash@icloud.com>
2023-04-02 09:52:28 -0700

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,29 +9,29 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className="flex items-center justify-center w-full h-full">
-				<div className="w-fit grid grid-cols-8 grid-rows-[repeat(9,minmax(0,1fr))] gap-0.5 bg-black border-2 border-black">
+			<main className="w-full h-full overflow-auto">
+				<div className="w-fit grid-rows-9 gap-lg grid grid-cols-8 bg-black border-2 border-black">
 					{Array.from({ length: 72 }).map((_, i) => (
 						<div key={i} className="bg-white">
-							<div className="grid grid-cols-4 grid-rows-2 gap-[1px] bg-black border-b border-black">
+							<div className="gap-sm grid grid-cols-4 grid-rows-2 bg-black border-b border-black">
 								{Array.from({ length: 8 }).map((_, i2) => (
-									<div key={i2} className="w-[25px] h-[20px] bg-white"></div>
+									<div key={i2} className="w-5 h-4 bg-white"></div>
 								))}
 							</div>
-							<div className="flex flex-row gap-[1px] bg-black">
-								<div className="w-[60px] h-10 bg-white">
-									<div className="w-fit grid grid-cols-2 gap-[1px] bg-black border-b border-r border-black">
-										<div className="w-[25px] h-[15px] bg-white"></div>
-										<div className="w-[25px] h-[15px] bg-white"></div>
+							<div className="gap-sm flex flex-row bg-black">
+								<div className="w-12 h-8 bg-white">
+									<div className="w-fit gap-sm grid grid-cols-2 bg-black border-b border-r border-black">
+										<div className="w-5 h-3 bg-white"></div>
+										<div className="w-5 h-3 bg-white"></div>
 									</div>
-									<div className="h-[25px]"></div>
+									<div className="h-5"></div>
 								</div>
-								<div className="flex items-center justify-center w-[42px] h-10 bg-white">
-									<div className="flex items-center justify-center w-10 h-10 rotate-45">
-										<div className="grid content-between justify-between w-5 h-5 grid-cols-[auto,auto] border border-black">
-											<div className="w-[5px] h-[5px] border-black border-r border-b"></div>
-											<div className="w-[5px] h-[5px] border-black border-l border-b"></div>
-											<div className="w-[5px] h-[5px] border-black border-r border-t"></div>
+								<div className="w-md flex items-center justify-center h-8 bg-white">
+									<div className="flex items-center justify-center w-8 h-8 rotate-45">
+										<div className="grid-cols-auto2 grid content-between justify-between w-5 h-5 border border-black">
+											<div className="w-1 h-1 border-b border-r border-black"></div>
+											<div className="w-1 h-1 border-b border-l border-black"></div>
+											<div className="w-1 h-1 border-t border-r border-black"></div>
 										</div>
 									</div>
 								</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
+let scale = 2;
+let spacing = {
+	1: 5 * scale + "px",
+	3: 15 * scale + "px",
+	4: 20 * scale + "px",
+	5: 25 * scale + "px",
+	8: 40 * scale + "px",
+	10: 50 * scale + "px",
+	12: 60 * scale + "px",
+	md: 42 * scale + "px",
+};
+let gap = {
+	sm: 1 * scale + "px",
+	lg: 2 * scale + "px",
+};
+
+console.log(spacing);
+
 module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				4: "repeat(4, 1fr)",
+				8: "repeat(8, 1fr)",
+				auto2: "auto, auto",
+			},
+			gridTemplateRows: {
+				2: "repeat(2, 1fr)",
+				9: "repeat(9, 1fr)",
+			},
+			gap: gap,
+			spacing: spacing,
+		},
 	},
 	plugins: [],
 };
