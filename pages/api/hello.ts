0b8c6342d30b3b7c99dda1af01b2321772a1602d
quincynash <quincy.nash@icloud.com>
2023-04-02 08:55:30 -0700

    commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../lib/prisma";
 
 type Data = {
-  name: string
-}
+	name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	console.log(await prisma.pitchers.count());
+	res.status(200).json({ name: "John Doe" });
 }
